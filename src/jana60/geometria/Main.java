9039071c0c0c7bac55400b957ec2a4f7071b010b
paololabriola <paololabriola02@gmail.com>
2022-06-14 14:24:31 +0200

    Exercise completed

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,10 +1,26 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Rettangolo figura1 = new Rettangolo();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Questo codice stamperà in output l'area e il perimetro di un rettangolo a partire da una base e un altezza date in input.");
+		
+		System.out.print("Inserisci il valore della base del rettangolo: ");
+		figura1.baseRettangolo = scan.nextFloat();
+		
+		System.out.print("Inserisci il valore dell'altezza del rettangolo: ");
+		figura1.altezzaRettangolo = scan.nextFloat();
+		
+		figura1.stampaRisultato();
 
+		scan.close();	
+		
 	}
 
 }
--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -2,4 +2,24 @@ package jana60.geometria;
 
 public class Rettangolo {
 
+	float baseRettangolo, altezzaRettangolo;
+	
+	float calcolaPerimetro(){
+		
+		return (baseRettangolo + altezzaRettangolo) * 2;
+		
+	}
+	
+	float calcolaArea() {
+		
+		return baseRettangolo * altezzaRettangolo;
+		
+	}
+	
+	void stampaRisultato() {
+		
+		System.out.println("Il perimetro del rettantolo inserito è pari a " + calcolaPerimetro() + ", l'area del rettangolo invece è pari a " + calcolaArea());
+		
+	}
+	
 }
