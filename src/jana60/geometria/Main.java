c4b01d4af5ce07981090992eaa8034b325f82ca2
paololabriola <paololabriola02@gmail.com>
2022-06-14 14:09:07 +0200

    first push

---
+++ src/jana60/geometria/Main.java
@@ -0,0 +1,10 @@
+package jana60.geometria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/jana60/geometria/Rettangolo.java
@@ -0,0 +1,5 @@
+package jana60.geometria;
+
+public class Rettangolo {
+
+}
